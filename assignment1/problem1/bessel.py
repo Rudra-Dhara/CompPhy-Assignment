eb75b2ae0179d9ca14d632d76efb82076329bc69
Rudra-Dhara <rudra.dhara1141@gmail.com>
2023-02-13 19:58:44 +0545

    updated recurrance relation

--- assignment1/problem1/bessel.py
+++ assignment1/problem1/bessel.py
@@ -4,18 +4,21 @@ def j0(x):
     return np.sin(x)/x
 def j1(x):
     return (np.sin(x)- x*np.cos(x))/x**2
-def dF(f,x,*arg):
-    h=1
-    return (f(x-2*h,*arg)-8*f(x-h,*arg)+8*f(x+h,*arg)-f(x+2*h,*arg))/(12*h)
+
 
 def j_up(x,l):
     if l>1:
-        return (l-1)*j_up(x,l-1)/x - dF(j_up,x,l-1)
+        return (2*l-1)*j_up(x,l-1)/x - j_up(x,l-2)
     elif l==1:    
         return j1(x)
     else:
         return j0(x)
     
-x=np.linspace(0.1,50,100)
-for i in x:
-    print(i,"   ",j_up(i,10))
+x=np.linspace(0,50,250)
+i=0
+plt.plot(x,j_up(x,i),label="{}".format(i))
+plt.legend()
+plt.show()
+
+
+
