98f7cdbb6ee105c11248efbb527d6dffe00adcf9
Rudra-Dhara <rudra.dhara1141@gmail.com>
2023-03-07 06:42:13 +0545

    final touch done

--- assignment2/prob1_diff_eq.py
+++ assignment2/prob1_diff_eq.py
@@ -1,7 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.sparse.linalg import spsolve
-from scipy import sparse
 
 #Here, a, b, and c are the diagonal, main diagonal, and subdiagonal entries, d is lhs of the eqn
 #this code written by taking the help of the internet
@@ -26,9 +24,6 @@ def tdma_solver(a, b, c, d):
     return x
 
 
-
-
-
 # Define the Poisson equation function
 def poi_fn(x):
     return -(3*x + x**2)*np.exp(x)
@@ -39,42 +34,63 @@ def exact(x):
 
 # Define the grid and step size h
 n_values = [10, 100, 1000, 10**4,10**6]
-max_errors = []
 
+
+#part a
+print('With this program we solve')
+list_of_errlst=[] # the lists to store the different values of u_list for different value
+list_of_xlst=[]
 for n in n_values:
-    h = 1/(n+1)
-    x = np.linspace(h, 1-h, n) #x values with different interval 
-
-    #the 
-    main_diag = -2*np.ones(n)
-    off_diag = np.ones(n)   
+    h = 1/(n)
+    x = np.linspace(0,1, n+1) #x values with different interval 
+    print(len(x))
+    #the tridiagnolan matrix diagonals
+    main_diag = -2*np.ones(n-1)
+    off_diag = np.ones(n-1)   
 
     # b vector
     b = h**2*poi_fn(x)
-
-    # Apply the boundary conditions
-    b[0] = 0
-    b[-1] = 0
+    
 
     #solving u
     u = tdma_solver(off_diag, main_diag, off_diag, b)
-
-    # Calculate the maximum relative error
-    max_error = np.max(np.abs((u-exact(x))/exact(x)))
-    max_errors.append(max_error)
+    u= list(u)
+    u.insert(0,0)
+    u.append(0)
+    
+    print(len(x),len(u))
+    # relative error list and x (storing the different lists due to different n within the loop)
+    u_err= np.log10(np.absolute((u- exact(x))/exact(x)))
+    u_err= list(u_err) #converting data type to list
+    x==list(x) # converting data tupe to list
+    list_of_errlst.append(u_err)
+    list_of_xlst.append(x)
 
     # Plot the numerical solution and the exact solution
     plt.plot(x, u, label='Numerical solution for {}'.format(n))
     
     
-# Tabulate the results
+# Part c
 plt.plot(x, exact(x), label='Exact solution')
 plt.legend()
+plt.xlabel('x')
+plt.ylabel('u(x)')
 plt.title('comparision b/w exact solution')
+plt.grid()
 plt.show()
 
-print('h\tmax error')
-for n, max_error in zip(n_values, max_errors):
-    h = 1/(n+1)
-    print(f'{h:.1e}\t{max_error:.8f}') 
+
+#Part d 
+for i in range(len(n_values)):
+    plt.plot(list_of_xlst[i],list_of_errlst[i],label=' error for n = {}'.format(n_values[i]))
+plt.title('The relative error(in log scale) vs x for different n')
+plt.xlabel('x')
+plt.ylabel('log_{10}(rel err)')
+plt.legend()
+plt.grid()
+plt.show()
+
+
     
+
+
