afbddb8329189710d690d93e75b6681ac8e4bf03
Rudra-Dhara <rudra.dhara1141@gmail.com>
2023-04-02 18:13:31 +0545

    2d start

--- assignment3/pr2_Intigrations.py
+++ assignment3/pr2_Intigrations.py
@@ -50,3 +50,51 @@ print('\n')
 for n in N:
     I = gauss_legendre(f, a, b, n)
     print(f"Gauss-Legendre quadrature with {n} mesh points: I = {I:.6f}")
+
+
+
+
+
+
+# Define the integrand function
+def integrand(x, y):
+    return 1 / (1 - x*y)
+
+# Define the limits of integration
+x_min, x_max = 0, 0.9999        #as the integral is blowing up at x=1 we put the limit si
+y_min, y_max = 0, 0.9999
+
+def trapz2d(f, a, b, c, d, nx=1000, ny=1000):
+    """Approximate the integral of f(x,y) over the domain [a,b] x [c,d] using
+    the composite trapezoidal rule with nx panels in x and ny panels in y."""
+    
+    # create the x and y mesh points
+    x = np.linspace(a, b, nx+1)
+    y = np.linspace(c, d, ny+1)
+    
+    # compute the step sizes in the x and y directions
+    hx = (b - a) / nx
+    hy = (d - c) / ny
+    
+    # initialize the sum
+    integral = 0.0
+    
+    # loop over the panels in the x direction
+    for i in range(nx):
+        # loop over the panels in the y direction
+        for j in range(ny):
+            # evaluate the function at the four corners of the panel
+            f00 = f(x[i], y[j])
+            f01 = f(x[i], y[j+1])
+            f10 = f(x[i+1], y[j])
+            f11 = f(x[i+1], y[j+1])
+            
+            # compute the integral over this panel using the trapezoidal rule
+            panel_integral = (hx*hy/4) * (f00 + f01 + f10 + f11)
+            
+            # add the panel integral to the total integral
+            integral += panel_integral
+    
+    return integral
+
+print(trapz2d(integrand,x_min,x_max,y_min,y_max))
