f6688a9061cbe1dfb373bef1cedf9b42e93cdfd5
Rudra-Dhara <rudra.dhara1141@gmail.com>
2023-04-08 18:45:41 +0545

    chi is set

--- assignment3/pr1_chi_min.py
+++ assignment3/pr1_chi_min.py
@@ -1,17 +1,50 @@
 import numpy as np
-from pr2_Intigration.py import gauss_legendre
+from scipy.optimize import minimize
+import math
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d 
+
+def gauss_legendre(f, a, b, n,*args):
+    x = [0.0] * n     # Roots of Legendre polynomial
+    w = [0.0] * n     # Weights for each root
+    xm = 0.5 * (b + a)   # Midpoint of interval [a,b]
+    xr = 0.5 * (b - a)   # Half-length of interval [a,b]
+    sum = 0     
+    for i in range(n):
+        # Compute Legendre polynomial using recurrence relation
+        z = math.cos(math.pi * (i + 0.75) / (n + 0.5))   # Root of Legendre polynomial
+        p1 = 1.0   # Legendre polynomial l=0
+        p2 = 0.0   # Legendre polynomial l=-1
+
+        #calculating the  n th odrer legendre
+        for j in range(1, n + 1):
+            p3 = p2   # Legendre polynomial for i=j-2 
+            p2 = p1   # Legendre polynomial for i=j-1
+            # Recurrence relation for Legendre polynomial 
+            p1 = ((2.0 * j - 1.0) * z * p2 - (j - 1.0) * p3) / j
+        # Compute derivative of Legendre polynomial
+        pp = n * (z * p1 - p2) / (z**2 - 1.0)       #taken from the book
+        # Compute root and weight
+        x[i] = xm - xr * z   # Root in interval [a,b]
+        w[i] = 2.0 * xr / ((1.0 - z * z) * pp**2)   # Weight for root
+
+        sum += w[i] * f(x[i],*args)   # Weighted value of integrand at root
+    return sum
 
 # Define the density of states function
-def rho(m, A, m0=0.5, TH):
+def rho(m, A, TH, m0=0.5):
     return A * (m**2 + m0**2)**(5/4) * np.exp(m/TH)
 
 # Define the theoretical number of states function
 def Ntheory(A, m0, TH, mmax):
-    return gauss_legendre(lambda m: rho(m, A, m0, TH), m0, mmax, n=10)
+    return gauss_legendre(lambda m: rho(m, A, m0, TH), 0, mmax, n=10)
 
 # Define the step function
-def step(x):
-    return 1.0 if x >= 0 else 0.0
+def step_fn(m, m_i):
+    if m >= m_i:
+        return 1
+    else:
+        return 0
 # declaring the value of g as global variable
 
 # Define the experimental number of states function for mesons
@@ -19,7 +52,7 @@ def Nexp_mesons(m,mes_list):
     g=3 # for meson
     sum = 0
     for i in mes_list:
-        sum+= g*(m - i)
+        sum+= g*step_fn(m,i)
 
     return sum
 
@@ -28,14 +61,15 @@ def Nexp_baryons(m, bar_list):
     g= 4 #for baryon
     sum = 0
     for i in bar_list:
-        sum+= g*(m - i)
+        sum+= g*step_fn(m,i)
 
     return sum
 
-def chi2(Nexp,exp_list,m,sigma_list,A,TH,mmax,m0=0.5):
+#defining chi**2 function as all sigma = 1 we eliminate it from the function
+def chi2(Nexp,exp_list,A,TH,m0=0.5):
     sum=0
-    for i in sigma_list:
-        sum+= (np.log10(Nexp(m,exp_list)) - np.log10(Ntheory(A, m0, TH, mmax)))/i
+    for m_i in exp_list:
+        sum+= (np.log10(Nexp(m_i,exp_list)) - np.log10(Ntheory(A, m0, TH, m_i)))**2
     
     return sum
 
@@ -44,4 +78,17 @@ def chi2(Nexp,exp_list,m,sigma_list,A,TH,mmax,m0=0.5):
 mes_mass = np.array([0.782, 1.170, 1.282, 1.420, 1.512])
 bar_mass = np.array([0.938, 1.440, 1.535, 1.650, 1.710])
 sigma=np.ones(5)
+
+
+
+# Define the chi-square function with fixed parameters
+def chi2_fixed(params):
+    A, TH = params
+    bar_mass = np.array([0.938, 1.440, 1.535, 1.650, 1.710])
+    return chi2(Nexp_baryons, bar_mass, A, TH, 0.5)
+
+
+
+
+
 
--- assignment3/pr2_Intigrations.py
+++ assignment3/pr2_Intigrations.py
@@ -5,6 +5,14 @@ import math
 def f(x):
     return 1/(2+x**2)
 
+
+# in the up coming function 
+# -------------------------
+# f - is the integrand function
+# a - lower limit of x integral
+# b - upper limit of x integral
+# n - number of division points
+
 # trapezoidal rule
 def trapezoidal(f, a, b, n):
     x = np.linspace(a, b, n+1)
@@ -34,104 +42,70 @@ def simpson(f,x0,xn,n,*args):
     return integration
 
 #  Gauss-Legendre quadrature
+# here the approximation for for root of legendre function is used and weight from derivative formula
 def gauss_legendre(f, a, b, n,*args):
     x = [0.0] * n     # Roots of Legendre polynomial
     w = [0.0] * n     # Weights for each root
     xm = 0.5 * (b + a)   # Midpoint of interval [a,b]
     xr = 0.5 * (b - a)   # Half-length of interval [a,b]
-    sum = 0.0     # Running sum of the integrand
+    sum = 0     
     for i in range(n):
-        m = (n + 1) // 2   # Index of midpoint
         # Compute Legendre polynomial using recurrence relation
         z = math.cos(math.pi * (i + 0.75) / (n + 0.5))   # Root of Legendre polynomial
-        p1 = 1.0   # Legendre polynomial for i=0
-        p2 = 0.0   # Legendre polynomial for i=-1 (not used)
+        p1 = 1.0   # Legendre polynomial l=0
+        p2 = 0.0   # Legendre polynomial l=-1
+
+        #calculating the  n th odrer legendre
         for j in range(1, n + 1):
-            p3 = p2   # Legendre polynomial for i=j-2 (not used)
+            p3 = p2   # Legendre polynomial for i=j-2 
             p2 = p1   # Legendre polynomial for i=j-1
-            # Recurrence relation for Legendre polynomial
+            # Recurrence relation for Legendre polynomial 
             p1 = ((2.0 * j - 1.0) * z * p2 - (j - 1.0) * p3) / j
         # Compute derivative of Legendre polynomial
-        pp = n * (z * p1 - p2) / (z * z - 1.0)
+        pp = n * (z * p1 - p2) / (z**2 - 1.0)       #taken from the book
         # Compute root and weight
         x[i] = xm - xr * z   # Root in interval [a,b]
-        w[i] = 2.0 * xr / ((1.0 - z * z) * pp * pp)   # Weight for root
-        # Update running sum
+        w[i] = 2.0 * xr / ((1.0 - z * z) * pp**2)   # Weight for root
+
         sum += w[i] * f(x[i],*args)   # Weighted value of integrand at root
     return sum
 
 
-
-# Define the limits of integration
-a, b = 0, 3
-
-# mesh points
-N = [10, 40, 100, 1000]
-
-# integrals using the trapezoidal rule
-for n in N:
-    I = trapezoidal(f, a, b, n)
-    print(f"Trapezoidal rule with {n} mesh points: I = {I:.6f}")
-print('\n')
-
-#using Simpson's rule
-for n in N:
-    I = simpson(f, a, b, n)
-    print(f"Simpson's rule with {n} mesh points: I = {I:.6f}")
-print('\n')
-
-# Gauss-Legendre quadrature
-for n in N:
-    I = gauss_legendre(f, a, b, n)
-    print(f"Gauss-Legendre quadrature with {n} mesh points: I = {I:.6f}")
-
-
-print('\n\n')
-
-
-
-# Define the integrand function
-def integrand(x, y):
-    return 1 / (1 - x*y)
-
-# Define the limits of integration
-x_min, x_max = 0, 0.9999        #as the integral is blowing up at x=1 we put the limit si
-y_min, y_max = 0, 0.9999
-
-def trapz2d(f, a, b, c, d, nx=1000, ny=1000):
-    """Approximate the integral of f(x,y) over the domain [a,b] x [c,d] using
-    the composite trapezoidal rule with nx panels in x and ny panels in y."""
+#defining function for double integrals
+# c - lower limit of y integral
+# d - upper limit of y integral
+# nx and ny - are number of division along x and y 
+# other arguments are same as above
+def trapz2d(f, a, b, c, d, nx, ny):
     
     # create the x and y mesh points
     x = np.linspace(a, b, nx+1)
     y = np.linspace(c, d, ny+1)
     
-    # compute the step sizes in the x and y directions
+    # step sizes in the x and y 
     hx = (b - a) / nx
     hy = (d - c) / ny
     
-    # initialize the sum
     integral = 0.0
     
-    # loop over the panels in the x direction
     for i in range(nx):
-        # loop over the panels in the y direction
+        
         for j in range(ny):
-            # evaluate the function at the four corners of the panel
+            # function at the four corners of the trapizoid
             f00 = f(x[i], y[j])
             f01 = f(x[i], y[j+1])
             f10 = f(x[i+1], y[j])
             f11 = f(x[i+1], y[j+1])
             
-            # compute the integral over this panel using the trapezoidal rule
-            panel_integral = (hx*hy/4) * (f00 + f01 + f10 + f11)
+            # volume of the box
+            box_integral = (hx*hy/4) * (f00 + f01 + f10 + f11)
             
-            # add the panel integral to the total integral
-            integral += panel_integral
+            integral += box_integral
     
     return integral
 
-
+#here we considered the whole integral along strips along x direction 
+# then we evaluate the total integral of strips along y direction
 def simpson_2d(f, a, b, c, d, Nx, Ny):
     hx = (b-a)/Nx
     hy = (d-c)/Ny
@@ -161,6 +135,39 @@ def gauss_legendre_2d(f, a, b, c, d, Nx, Ny):
         I += wp[i]*gauss_legendre(f,a,b,Nx,yp[i])
     return I
 
+# Define the limits of integration
+a, b = 0, 3
+
+# mesh points
+N = [10, 40, 100, 1000]
+
+# integrals using the trapezoidal rule
+for n in N:
+    I = trapezoidal(f, a, b, n)
+    print(f"Trapezoidal rule with {n} mesh points: I = {I:.6f}")
+print('\n')
+
+#using Simpson's rule
+for n in N:
+    I = simpson(f, a, b, n)
+    print(f"Simpson's rule with {n} mesh points: I = {I:.6f}")
+print('\n')
+
+# Gauss-Legendre quadrature
+for n in N:
+    I = gauss_legendre(f, a, b, n)
+    print(f"Gauss-Legendre quadrature with {n} mesh points: I = {I:.6f}")
+
+
+print('\n\n')
+# Define the integrand function
+def integrand(x, y):
+    return 1 / (1 - x*y)
+
+# Define the limits of integration
+x_min, x_max = 0, 0.9999        #as the integral is blowing up at x=1 we put the limit si
+y_min, y_max = 0, 0.9999
+#double integral's calculation
 for n in N:
     I = trapz2d(integrand,x_min,x_max,y_min,y_max,n,n)
     print(f"Trapezoidal 2d rule with {n} mesh points: I = {I:.6f}")
