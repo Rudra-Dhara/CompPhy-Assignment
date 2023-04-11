5d465f9f9be1b173cfa090f14d057a8a2d8d9ab9
Rudra-Dhara <rudra.dhara1141@gmail.com>
2023-04-12 02:51:01 +0545

    code is workint

--- assignment4/pr1_decay_mntcrl.py
+++ assignment4/pr1_decay_mntcrl.py
@@ -1,40 +1,56 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Constants
-lamb = 0.3  # Decay constant (sec^-1)
-dt = 1e-4       # Time step (sec)
-Nruns = 1000    # Number of runs/events
+import numpy as np
 
-# Initial number of nuclei
-N0_list = [10, 100, 1000]
+def rad_decay(N0:int, lambda_, dt, Nruns):
+    #initial conditions
+    Nt= N0
+    
+    t_list_decay=[]
+    Nt_list= []  #NbyM array
+    t_list = []   #nbymarray
+    for i in range(Nruns):
+        count=0
+        Nt_l=[]
+        t_l= []
+        while Nt>0 :
+            p= lambda_ *dt*Nt #probability
+            #generating random number b/w 0 & 1
+            rand_x = np.random.rand()
 
-# Function to simulate radioactive decay
-def radioactive_decay(N0, lamb, dt, Nruns):
-    times = np.arange(0, Nruns*dt, dt)  # Time array
-    Nt = np.zeros(len(times))           # Number of nuclei array
-    Nt[0] = N0                          # Set initial number of nuclei
-    for i in range(1, len(times)):
-        dN = -lamb * Nt[i-1] * dt    # Decay rate
-        Nt[i] = Nt[i-1] + dN            # Update number of nuclei
-    return times, Nt
+            if rand_x <= p:
+                Nt-=1   # one particle decay if rand_x satasfy the probability condition
+            count+=1
+            Nt_l.append(Nt)
+            t_l.append(count*dt)
+        Nt_list.append(Nt_l)
+        t_list.append(t_l)
+    
+    return t_list, Nt_list
 
-# Perform simulations for different initial N0 values
-for N0 in N0_list:
-    avg_Nt = np.zeros(len(radioactive_decay(N0, lambda_, dt, Nruns)[1]))
-    for j in range(Nruns):
-        Nt = radioactive_decay(N0, lambda_, dt, Nruns)
-        avg_Nt += Nt
-    avg_Nt /= Nruns
+# Parameters
+N0_values = [10, 100, 1000] # Initial number of nuclei
+lambda_ = 0.3 # Decay constant
+dt = 0.5* 10**(-3) # Time step
+Nruns = 1000 # Number of runs/events
 
-    # _Plot t vs. ln(Nt)
-    plt.plot(radioactive_decay(N0, lambda_, dt, Nruns)[0], np.log(avg_Nt), label=f'N0 = {N0}')
+for N0 in N0_values:
+    t_list, Nt_list = rad_decay(N0,lambda_,dt,Nruns)
+    max_Nt= max(Nt_list,key=len)
+    max_t= max(t_list,key=len)
 
-# Plot exact result
-t_exact = np.linspace(0, Nruns*dt, num=1000)
-N_exact = N0 * np.exp(-lambda_ * t_exact)
-plt.plot(t_exact, np.log(N_exact), 'r', label='Exact')
-plt.xlabel('Time (s)')
-plt.ylabel('ln(N(t))')
-plt.legend()
+    # Pad the sublists with zeros
+    for sublist in Nt_list:
+        sublist.extend([0] * (len(max_Nt) - len(sublist)))
+
+    Nt_list= np.array(Nt_list)  #converting it to numpy array
+
+    Nt_avg=np.zeros(len(max_Nt))
+    for sl in Nt_list:
+        Nt_avg+= sl
+    
+    Nt_avg =Nt_avg/Nruns
+
+    plt.plot(max_t,Nt_avg)
 plt.show()
