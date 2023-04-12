4cf2791e3b5427bd286063ab560507d0a9f70ef2
Rudra-Dhara <rudra.dhara1141@gmail.com>
2023-04-12 17:27:19 +0545

    finished with comments

--- assignment4/pr2_mntcrl_inti.py
+++ assignment4/pr2_mntcrl_inti.py
@@ -1,28 +1,28 @@
 import numpy as np
 
-# Define the integrand function
+# integrand function
 def integrand(x):
     return np.exp(-x**2)
 
-# Define the probability distribution function for importance sampling
+# PDF for importance sampling
 def p(x, A):
     return A * np.exp(-x)
 
-# Define the limits of integration
+# integration limit
 a = 0
 b = 1
 
-# Define the values of N for Monte Carlo sampling
+# values of N for Monte Carlo sampling
 N_values = [10**3, 10**4, 10**5]
 
-# Loop over different values of N
+# loop over different values of N
 for N in N_values:
-    # (a) Brute force Monte Carlo
+    # (a) brute force Monte Carlo
     x_brute = np.random.rand(N) # Generate random numbers from uniform distribution
     integral_brute = np.mean(integrand(x_brute*(b-a))*(b-a))  # Estimate the integral using the mean of function evaluations
     brute_var = np.var(integrand(x_brute*(b-a))*(b-a))
 
-    # (b) Importance sampling with p(x) = Ae**-x
+    # (b) importance sampling with p(x) = Ae**-x
     # the transformed variable y(x) is = - ln (1 - x/A)
     A = 1 / (1 - np.exp(-b)) # Calculate the value of A to normalize p(x)
     x_imp = -np.log(np.ones_like(x_brute)-x_brute/A) # change of variavle
