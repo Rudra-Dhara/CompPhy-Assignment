4dedca534f52f87b1fdcadb1d77a36cb1cbccc66
Rudra-Dhara <rudra.dhara1141@gmail.com>
2023-04-13 06:38:14 +0545

    generalized limit

--- assignment4/pr2_mntcrl_inti.py
+++ assignment4/pr2_mntcrl_inti.py
@@ -19,12 +19,12 @@ N_values = [10**3, 10**4, 10**5]
 for N in N_values:
     # (a) brute force Monte Carlo
     x_brute = np.random.rand(N) # Generate random numbers from uniform distribution
-    integral_brute = np.mean(integrand(x_brute*(b-a))*(b-a))  # Estimate the integral using the mean of function evaluations
-    brute_var = np.var(integrand(x_brute*(b-a))*(b-a))
+    integral_brute = np.mean(integrand(a+ x_brute*(b-a))*(b-a))  # Estimate the integral using the mean of function evaluations
+    brute_var = np.var(integrand(a+ x_brute*(b-a))*(b-a))
 
     # (b) importance sampling with p(x) = Ae**-x
     # the transformed variable y(x) is = - ln (1 - x/A)
-    A = 1 / (1 - np.exp(-b)) # Calculate the value of A to normalize p(x)
+    A = 1 / (np.exp(-a) - np.exp(-b)) # Calculate the value of A to normalize p(x)
     x_imp = -np.log(np.ones_like(x_brute)-x_brute/A) # change of variavle
     integral_imp = np.mean(integrand(x_imp) / p(x_imp, A)) # Estimate the integral using importance sampling
     imp_var= np.var(integrand(x_imp) / p(x_imp, A))
